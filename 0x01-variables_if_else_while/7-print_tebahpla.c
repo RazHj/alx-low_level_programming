fb06f75ec8b528d7651187b890c0f890d64d2ab0
RazHj <razanhalaj@gmail.com>
2023-09-08 18:12:38 -0700

    fix

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabets in lowercase, and then in uppercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
