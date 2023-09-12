e979150e8919b1deea3711b7eac788a1741ab6dc
RazHj <razanhalaj@gmail.com>
2023-09-12 10:17:40 -0700

    fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print lowercase letters
+* Description: this programm prints '_putchar'
+* Return: always return 0
+**/
+void print_alphabet_x10(void)
+{
+	char x, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
