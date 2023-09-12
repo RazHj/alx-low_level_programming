48adcfb0b97a96287587cfaa0f32b6b6894ff8fd
RazHj <razanhalaj@gmail.com>
2023-09-12 10:10:42 -0700

    fix

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - print lowercase letters
+* Description: this programm prints '_putchar'
+* Return: always return 0
+**/
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
